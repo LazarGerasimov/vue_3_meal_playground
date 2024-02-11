078ad91889e0c0c7c3ab968dd5694d8b7b06bd55
Laz <lazgerasimov@gmail.com>
2024-02-11 23:01:53 +0200

    add home input scaff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
-  Home
+  <div class="flex flex-col p-8">
+    <input type="text" class="w-full rounded border-2 border-gray-200" placeholder="Search for meals.." />
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link to="/" v-for="letter of letters">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from 'vue';
+import store from '../store'
+
+const meals = computed(() => store.state.meals);
+
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+
 
 </script>
 
