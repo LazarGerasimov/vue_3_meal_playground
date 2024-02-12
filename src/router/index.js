37cf22120a343df1a032443df185f161c100873d
Laz <lazgerasimov@gmail.com>
2024-02-12 12:59:38 +0200

    add default layout

--- src/router/index.js
+++ src/router/index.js
@@ -5,29 +5,37 @@ import Home from '../views/Home.vue';
 import MealsByName from '../views/MealsByName.vue'
 import MealsByLetter from '../views/MealsByLetter.vue'
 import MealsByIngredient from '../views/MealsByIngredient.vue'
+import DefaultLayout from '../components/DefaultLayout.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: Home
+    path: '/',
+    component: DefaultLayout,
+    children: [
+      {
+        path: "/",
+        name: "home",
+        component: Home
+      },
+      {
+        path: "/by-letter/:letter?",
+        name: "byLetter",
+        component: MealsByLetter
+      },
+      {
+        path: "/by-name/:name?",
+        name: "byName",
+        component: MealsByName
+      },
+      {
+        path: "/by-ingredient/:ingredient?",
+        name: "byIngredient",
+        component: MealsByIngredient
+      },
+    ]
   },
-  {
-    path: "/by-letter/:letter?",
-    name: "byLetter",
-    component: MealsByLetter
-  },
-  {
-    path: "/by-name/:name?",
-    name: "byName",
-    component: MealsByName
-  },
-  {
-    path: "/by-ingredient/:ingredient?",
-    name: "byIngredient",
-    component: MealsByIngredient
-  },
-  
+
+
 ];
 
 const router = createRouter({
