b045834e6a43f086a70a68c2eaa7940fc8a85511
Laz <lazgerasimov@gmail.com>
2024-02-12 14:08:23 +0200

    fetch meals by keyword; store them in vuex

---
+++ src/store/mutations.js
@@ -0,0 +1,6 @@
+
+
+
+export function setSearchedMeals(state, meals) {
+  state.searchedMeals = meals
+}
--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,7 +1,26 @@
 
 <template>
   <div class="p-8">
-    <input type="text" class="w-full rounded border-2 border-gray-200" placeholder="Search for meals.." />
+    <input v-model="keyword" type="text" class="w-full rounded border-2 border-gray-200" placeholder="Search for meals.."
+      @change="searchMeals" />
+  </div>
+
+  <div>
+    <pre>{{ meals }}</pre>
   </div>
 </template>
-<script setup></script>
+
+<script setup>
+
+import { ref } from 'vue';
+import store from '../store';
+import { computed } from '@vue/reactivity';
+
+const keyword = ref('');
+const meals = computed(() => store.state.searchedMeals);
+
+function searchMeals() {
+  store.dispatch('searchMeals', keyword.value)
+}
+
+</script>
