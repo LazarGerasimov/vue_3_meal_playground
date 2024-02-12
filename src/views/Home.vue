79b6e0dcc86d57a9a32a8b9bbb469ff63b947b56
Laz <lazgerasimov@gmail.com>
2024-02-12 10:14:24 +0200

    create navigation scaffold

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,16 +10,17 @@
 </template>
 
 <script setup>
-import { computed, onMounted } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import axiosClient from '../axiosClient';
 
 
 const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
+const ingredients = ref([]);
 
 onMounted(async () => {
   const response = await axiosClient.get('/list.php?i=list')
   console.log(response.data)
-
+  ingredients.value = response.data
 })
 
 
