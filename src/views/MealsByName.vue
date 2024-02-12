1b72fbc00559b9a5fcb426aaccce5c0c819eca58
Laz <lazgerasimov@gmail.com>
2024-02-12 13:01:42 +0200

    move search input to separate comp

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,7 +1,7 @@
 
 <template>
-  <div>
-    By Name
+  <div class="p-8">
+    <input type="text" class="w-full rounded border-2 border-gray-200" placeholder="Search for meals.." />
   </div>
 </template>
 <script setup></script>
