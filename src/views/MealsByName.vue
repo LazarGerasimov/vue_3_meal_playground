cc8927cc6e3871259f830f934b5c508790dd5357
Laz <lazgerasimov@gmail.com>
2024-02-12 14:29:57 +0200

    upd meals page styles

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -5,8 +5,24 @@
       @change="searchMeals" />
   </div>
 
-  <div>
-    <pre>{{ meals }}</pre>
+  <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+    <div v-for="meal of meals" :key="meal.idMeal" class="bg-white shadow rounded-xl">
+      <img :src="meal.strMealThumb" :alt="meal.strMeal" class="rounded-t-xl w-full h-56 object-cover">
+      <div class="px-3">
+        <h3 class="py-3 font-semibold text-center">{{ meal.strMeal }}</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae non porro voluptatem alias delectus
+          exercitationem nulla.
+        </p>
+        <div class=" py-2 flex items-center justify-between px-10">
+          <a :href="meal.strYoutube" target="_blank" class="px-3 py-2 rounded border-2 border-red-500  bg-red-500 hover:bg-red-600 text-white transition-colors">
+            Video
+          </a>
+          <router-link to="/" class="px-3 py-2 rounded border-2 border-blue-500  bg-blue-500 hover:bg-blue-600 text-white transition-colors">
+            View
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
